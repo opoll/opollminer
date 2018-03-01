10ce60c7b0df1bf3182236569dafdbdb45725629
Zack <zack13254@gmail.com>
2018-02-28 21:53:11 -0500

    mining-cli refactoring code improvements

--- minerclient/helpers.js
+++ minerclient/helpers.js
@@ -1,16 +1,15 @@
+
+// Get all app wide dependencies
 ﻿exports.http = require('http');
 exports.url = require('url');
 exports.level = require("level");
 exports.bodyParser = require('body-parser');
 exports.readline = require('readline');
-
-var chalk = require("chalk");
-exports.chalk = chalk;
-
+exports.chalk = require("chalk");
 
 /* echo [DEBUG] to console if .env has ISDEBUG set to 1 */
 exports.log = function (msg) {
     if (process.env.ISDEBUG == 1) {
-        console.log(chalk.redBright('[DEBUG]'), msg);
+        console.log(exports.chalk.redBright('[DEBUG]'), msg);
     }
 }
