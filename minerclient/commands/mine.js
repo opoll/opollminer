ad797ed243bd77ce58897a099cdb88876e3ea991
gitsomedev <36387766+gitsomedev@users.noreply.github.com>
2018-03-08 21:22:40 -0500

    updated shard miner

---
+++ minerclient/commands/mine.js
@@ -0,0 +1,22 @@
+﻿var helpers = require("../helpers");
+
+// test shardid 8b64d8cb790cb33731bdd67e88f0c2dc46f01fbb5c0b3bf1dafe52af110224f8
+module.exports = function (CLI, ShardLogicController) {
+
+    CLI.AddCommand("mine", async function (args) {
+
+        var pollHash = args[1];
+        var activeShards = await ShardLogicController.ActiveShardsModule.getActiveShards();
+      //  console.log(activeShards);
+
+        if (activeShards[pollHash] == undefined) {
+            helpers.log("Could not find hash in active shard list");
+            return;
+        }
+        await ShardLogicController.WorkedShardsModule.persistMineShard(pollHash);
+
+        await ShardLogicController.POWController.StartMining(pollHash); // initiate cpp miner for this pollhash
+        //console.log(ShardLogicController.POWController);
+        
+    }, "Start mining a specified shard");
+}
