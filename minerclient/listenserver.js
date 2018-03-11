f52d72d442313b194c09066c056c8a9adbeb3a37
Zack <zack13254@gmail.com>
2018-03-10 22:35:21 -0500

    remove deprecated code

--- minerclient/listenserver.js
+++
@@ -1,9 +0,0 @@
-﻿'use strict';
-
-module.exports = function (app) {
-    // route commands
-    var routes = require('./routes/routes');
-   
-   //incoming commands
-    app.route('/shard/peers').get(routes.getPeers);
-};
