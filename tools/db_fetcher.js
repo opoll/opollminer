c799eddb6f03129e608d560125c504feab2e55b2
Benyam Ephrem <ephremdsg@gmail.com>
2018-03-18 02:02:34 -0400

    More dummy data added adhering to schemas (besides keys, sigs, and addresses being spoofed). Loader and fetcher for db testing functional as well. Call 'npm run wlf' to run the wipe, load, then fetch on the db to observe data contents.

--- tools/db_fetcher.js
+++ tools/db_fetcher.js
@@ -7,13 +7,91 @@ const walletIds = [
   "13fb9f0d937312a6787e70161f1ee1e5f8e0d3c48cc8b030a5063849310d390e"
 ]
 
-function getWallets(){
+const pollIds = [ // aka poll hash...poll hash used to map in db
+  "5F5FB96153BB67BCDFC0A95F8B98CF4E155AFD5D89A47E643E2509DC87ABCABF"
+]
+
+const shardBlockHashes = { // key is pollHash, value is the hashes of blocks in this shard in cardinal order
+  "5F5FB96153BB67BCDFC0A95F8B98CF4E155AFD5D89A47E643E2509DC87ABCABF": [
+    "46750073417EC82F7FC612B29519B9A3728025648A87EC89D4DFF1EE68DC907D",
+    "FABF1DFF25D72BECA4BEC55AE6E7E7127E02A48B9BFA0199E64B480A46538E6B"
+  ]
+}
+
+const randomPollId = pollIds[ Math.floor( Math.random() * pollIds.length ) ];
+
+// Fetches a random wallet from the Wallets db
+function getWalletsData(){
   const randomWalletId = walletIds[ Math.floor( Math.random() * walletIds.length ) ];
   databases.Wallets.get(randomWalletId, function(err, walletJson){
-    if (err) return console.log('Error loading wallet!', err)
+    if (err) return console.log('Error loading WalltsDb!', err)
     const wallet = JSON.parse(walletJson);
+
+    console.log("\n");
+    console.log("Wallets Db (Random Id Fetched)\n");
     console.dir(wallet);
+    console.log("\n");
   });
 }
 
-getWallets();
+// Fetches a random poll/poll object from the Polls db
+function getPollsData(){
+  databases.Polls.get(randomPollId, function(err, pollDataJson){
+    if (err) return console.log('Error loading PollsDb!', err)
+    const pollData = JSON.parse(pollDataJson);
+
+    console.log("\n");
+    console.log("Polls Db (Random Id Fetched)\n");
+    console.dir(pollData);
+    console.log("\n");
+  });
+}
+
+// Fetches a random poll/poll object from the Shards db (uses same randomPollId as getPollsData() fetch)
+function getShardsData(){
+  databases.Shards.get(randomPollId, function(err, shardDataJson){
+    if (err) return console.log('Error loading ShardsDb!', err)
+    const shardData = JSON.parse(shardDataJson);
+
+    console.log("\n");
+    console.log("Shards Db (Random Id Fetched)\n");
+    console.dir(shardData);
+    console.log("\n");
+  });
+}
+
+// Fetches a random shard/block object from the ShardBlocks db (uses same randomPollId as getPollsData() fetch)
+// then picks a random block hash to fetch the value for
+function getShardBlocksData(){
+  const hashes = shardBlockHashes[randomPollId]; // Get the block hashes for the random pollId/pollHash
+  const randomBlockHash = hashes[ Math.floor( Math.random() * hashes.length ) ];
+  databases.ShardBlocks.get(randomBlockHash, function(err, blockJson){
+    if (err) return console.log('Error loading ShardBlocksDb!', err)
+    const block = JSON.parse(blockJson);
+
+    console.log("\n");
+    console.log("ShardBlocks Db (Random Block Fetched)\n");
+    console.dir(block);
+    console.log("\n");
+  });
+}
+
+function getResponsePoolData(){
+  databases.ResponsePool.get(randomPollId, function(err, poolJson){
+    if (err) return console.log('Error loading ResponsePoolDb!', err)
+    const pool = JSON.parse(poolJson);
+
+    console.log("\n");
+    console.log("ResponsePool Db (Random Id Fetched)\n");
+    console.dir(pool);
+    console.log("\n");
+  });
+}
+
+
+// Call fetch functions
+getWalletsData();
+getPollsData();
+getShardsData();
+getShardBlocksData();
+getResponsePoolData();
--- tools/db_loader.js
+++ tools/db_loader.js
@@ -5,10 +5,15 @@ const R = require('ramda');
 const databases = require('../lib/util/databases');
 
 const testJson = {
-  Wallets: 'test/data/wallets.json'
+  Wallets: 'test/data/wallets.json',
+  Polls: 'test/data/polls.json',
+  Shards: 'test/data/shards.json',
+  ShardBlocks: 'test/data/shardBlocks.json',
+  ResponsePool: 'test/data/responsePool.json'
 };
 
-function loadWallets(){
+// Load 'Wallets' database with dummy data
+function loadWalletsDb(){
   // Load wallets json as js object
   const wallets = JSON.parse(fs.readFileSync(testJson.Wallets, 'utf8'));
 
@@ -25,4 +30,80 @@ function loadWallets(){
   R.forEachObjIndexed(storeWallet, wallets);
 }
 
-loadWallets();
+// Load 'Polls' database with dummy data
+function loadPollsDb(){
+  // Load polls data json as js object
+  const pollsData = JSON.parse(fs.readFileSync(testJson.Polls, 'utf8'));
+
+  const storePollsData = function(value, key){
+    // Load polls data
+    databases.Polls.put(key, JSON.stringify(value), function(err){
+      if (err) return console.log(`Error loading general data for poll with id ${key}`, err);
+      console.log(`Loaded general data for poll with id ${key}`);
+    });
+  }
+
+  // Call function on each item
+  R.forEachObjIndexed(storePollsData, pollsData);
+}
+
+// Load 'Shards' database with dummy data
+function loadShardsDb(){
+  // Load shards data json as js object
+  const shardsData = JSON.parse(fs.readFileSync(testJson.Shards, 'utf8'));
+
+  const storeShardsData = function(value, key){
+    // Load shards data
+    databases.Shards.put(key, JSON.stringify(value), function(err){
+      if (err) return console.log(`Error loading general data for shard with id ${key}`, err);
+      console.log(`Loaded general shard data for poll with id ${key}`);
+    });
+  }
+
+  // Call function on each item
+  R.forEachObjIndexed(storeShardsData, shardsData);
+}
+
+
+// Load 'ShardBlocks' database with dummy data
+function loadShardBlocksDb(){
+  // Load shards block data json as js object
+  const shardBlocksData = JSON.parse(fs.readFileSync(testJson.ShardBlocks, 'utf8'));
+
+  const storeShardBlocksData = function(value, key){
+    // Load shard blocks
+    databases.ShardBlocks.put(key, JSON.stringify(value), function(err){
+      if (err) return console.log(`Error loading shard block with hash ${key}`, err);
+      console.log(`Loaded shard block with id ${key}`);
+    });
+  }
+
+  // Call function on each item
+  R.forEachObjIndexed(storeShardBlocksData, shardBlocksData);
+}
+
+
+// Load 'ResponsePool' database with dummy data
+function loadResponsePoolDb(){
+  // Load pool data json as js object
+  const poolData = JSON.parse(fs.readFileSync(testJson.ResponsePool, 'utf8'));
+
+  const storePoolData = function(value, key){
+    // Load pool
+    databases.ResponsePool.put(key, JSON.stringify(value), function(err){
+      if (err) return console.log(`Error loading response pool for poll w/ hash ${key}`, err);
+      console.log(`Loaded response poll for poll w/ hash ${key}`);
+    });
+  }
+
+  // Call function on each item
+  R.forEachObjIndexed(storePoolData, poolData);
+}
+
+
+// Call load functions
+loadWalletsDb();
+loadPollsDb();
+loadShardsDb();
+loadShardBlocksDb();
+loadResponsePoolDb();
